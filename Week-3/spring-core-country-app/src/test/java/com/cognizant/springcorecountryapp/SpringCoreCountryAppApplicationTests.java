89d22a3da0c9c09fb4c7e9df257e2f5f0aa8c9d6
Nalarasu <nallarasum50@gmail.com>
2026-07-01 14:07:51 +0530

    Added Week-3 assignments

---
+++ Week-3/spring-core-country-app/src/test/java/com/cognizant/springcorecountryapp/SpringCoreCountryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.springcorecountryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreCountryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
